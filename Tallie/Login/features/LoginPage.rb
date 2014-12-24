cb35994275309d41e466cb80e8362c8f98de27fc
Midori Wasada <midoriwasada@Midoris-iMac.local>
2014-12-23 18:00:26 -0800

    BasePage is introduced

--- Tallie/Login/features/LoginPage.rb
+++ Tallie/Login/features/LoginPage.rb
@@ -1,26 +1,34 @@
-class LoginPage
+
+class LoginPage < BasePage
 
   USERNAME_INPUT = {id: 'Email'}
   PASSWORD_INPUT = {id: 'Password'}
   SUBMIT_BUTTON = {class: 'btn btn-success'}
+  SIGN_IN_H2 = {class: 'login-icon'}  ## Unable to find a good page element locator.  Ask dev to add an id to the h2 tag
 
   def initialize(browser)
-    @browser = browser
-
+    super
+    @selenium_driver = @browser.driver
+    visit(page_half_url)
+    @browser.wait_until(30, 'Timed out!Unable to get to Login page!') {@selenium_driver.find_element(SIGN_IN_H2).displayed?}
   end
 
-  def page_url
+  def page_half_url
     #TODO: set domain name in ENV variable
-    'https://alpha-springaheadgo.com/sso/Account/Logon'
-  end
-  def visit
-    @browser.goto(page_url)
+    '/sso/Account/Logon'
   end
 
-  def login
-    @browser.text_field(USERNAME_INPUT).set 'midori@springahead.com'
-    @browser.text_field(PASSWORD_INPUT).set 'Gomidogogo7711'
-    @browser.button(SUBMIT_BUTTON).click
+  # def visit
+  #   @browser.goto(page_url)
+  # end
+
+  def login(email = 'midori@springahead.com', password = 'Gomidogogo7711')
+    #TODO: Move valid user account to account yml file
+    #TODO: Need think about how we can ensure the account exists?
+
+    type(email, USERNAME_INPUT)
+    @selenium_driver.find_element(PASSWORD_INPUT).set password
+    @selenium_driver.find_element(SUBMIT_BUTTON).click
   end
 
 end
--- Tallie/step_definitions/common_steps.rb
+++ Tallie/step_definitions/common_steps.rb
@@ -7,7 +7,6 @@ Given /^I am on (Purchases|Free Trial|Login) page$/ do |page_name|
   case page_name
     when 'Free Trial', 'Login'
       @tallie_page = Object.const_get("#{page_name.gsub(" ","")}Page").new(@browser)
-      @tallie_page.visit
     when 'Purchases'
 
     else
